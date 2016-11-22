80613d0bb5cb4d3e45d8f15fc4d2ea35c2a45787
johannasamuelsen <johanna.samuelsen@gmail.com>
2016-11-22 11:51:03 +0100

    Trying to create identity database and first user Admin@admin.com once

--- src/ProjectFood/Controllers/UserController.cs
+++ src/ProjectFood/Controllers/UserController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using ProjectFood.Models.Entities;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using ProjectFood.Models.ViewModels;
 
 namespace ProjectFood.Controllers
 {
@@ -24,9 +26,49 @@ namespace ProjectFood.Controllers
             identityContext = identitycontext;
 
         }
-        public IActionResult Index()
+
+        public string Index()
+        {
+            return $"You are logged in as {User.Identity.Name}";
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult Login()
         {
             return View();
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginVM viewModel, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(viewModel);
+
+            // Skapa DB-schemat
+            await identityContext.Database.EnsureCreatedAsync();
+
+            // Create user
+            var user = new IdentityUser("Admin@admin.com");
+            var result = await userManager.CreateAsync(user, "Sommar2016!");
+
+            //var result = await signInManager.PasswordSignInAsync(
+            //    viewModel.Email, viewModel.Password, false, false);
+
+            //if (!result.Succeeded)
+            //{
+            //    ModelState.AddModelError(nameof(UserLoginVM.Email),
+            //        "Incorrect login credentials");
+            //    return View(viewModel);
+            //}
+
+            //if (string.IsNullOrWhiteSpace(returnUrl))
+            //    return RedirectToAction(nameof(UserController.Index));
+            //else
+            //    return Redirect(returnUrl);
+
+            return Redirect(returnUrl);
         }
     }
 }
---
+++ src/ProjectFood/Models/ViewModels/UserLoginVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectFood.Models.ViewModels
+{
+    public class UserLoginVM
+    {
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Enter an email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Enter a password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+    }
+}
