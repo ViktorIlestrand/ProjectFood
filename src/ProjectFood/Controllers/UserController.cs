5103cd558f4d5290cc6e924059c9d2e68a589053
Viktor <Administrator@ACADEMY-6211VVS>
2016-11-22 11:25:48 +0100

    fefe

--- src/ProjectFood/Controllers/UserController.cs
+++ src/ProjectFood/Controllers/UserController.cs
@@ -4,17 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjectFood.Models.Entities;
-
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProjectFood.Controllers
 {
     public class UserController : Controller
     {
+        UserManager<IdentityUser> userManager;
+        SignInManager<IdentityUser> signInManager;
+        IdentityDbContext identityContext;
         PatoDBContext context;
 
-        public UserController(PatoDBContext context)
+        public UserController(PatoDBContext context, UserManager<IdentityUser> usermanager, SignInManager<IdentityUser> signinmanager, IdentityDbContext identitycontext)
         {
+            userManager = usermanager;
+            signInManager = signinmanager;
             this.context = context;
+            identityContext = identitycontext;
+
         }
         public IActionResult Index()
         {
--- src/ProjectFood/Startup.cs
+++ src/ProjectFood/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using ProjectFood.Models.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace ProjectFood
 {
@@ -18,8 +19,18 @@ namespace ProjectFood
         {
             var connString = @"Server=tcp:patodb.database.windows.net,1433;Initial Catalog=PatoDB;Persist Security Info=False;User ID=PatoDBAdmin;Password=Sommar2016!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
-            services.AddDbContext<PatoDBContext>(
+            services.AddDbContext<IdentityDbContext>(
                 options => options.UseSqlServer(connString));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options => {
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Cookies.ApplicationCookie.LoginPath = "/user/login";
+            })
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
 
             services.AddMvc();
 
@@ -28,6 +39,7 @@ namespace ProjectFood
         public void Configure(IApplicationBuilder app)
         {
             app.UseDeveloperExceptionPage();
+            app.UseIdentity();
             app.UseMvcWithDefaultRoute();
         }
     }
