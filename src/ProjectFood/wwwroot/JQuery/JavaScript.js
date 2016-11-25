fb9e81197c4efa9ee04de17d479861fb8cc71446
Malin <malinulriksson@gmail.com>
2016-11-25 13:33:28 +0100

    javascript för att trycka på knapp till lista

---
+++ src/ProjectFood/wwwroot/JQuery/JavaScript.js
@@ -0,0 +1,11 @@
+﻿//Defining a listener for our button, specifically, an onclick handler
+document.getElementById("addFood").onclick = function () {
+    //First things first, we need our text:
+    var text = document.getElementById("newFood").value; //.value gets input values
+
+    var node = document.createElement("li");
+    var textNode = document.createTextNode(text);
+    node.appendChild(textNode);
+    //Now use appendChild and add it to the list!
+    document.getElementById("myKitchenList").appendChild(node);
+}
