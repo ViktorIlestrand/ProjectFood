4c7d6443bbdfbc34df393e5927556e1d93eba6c9
Viktor <Administrator@ACADEMY-6211VVS>
2016-11-30 11:20:54 +0100

    SMåändringar

---
+++ src/ProjectFood/Models/ViewModels/AdminVM/AddFoodItemVM.cs
@@ -0,0 +1,27 @@
+﻿using ProjectFood.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectFood.Models.ViewModels.AdminVM
+{
+    public class AddFoodItemVM
+    {
+        public string Name { get; set; }
+        public int FoodTypeId { get; set; }
+
+        public List<FoodType> ListOfFoodTypes { get; set; }
+
+        public AddFoodItemVM()
+        {
+
+        }
+        public AddFoodItemVM(string name, int foodTypeId, List<FoodType> listOfFoodTypes)
+        {
+            Name = name;
+            FoodTypeId = foodTypeId;
+            ListOfFoodTypes = listOfFoodTypes;
+        }
+    }
+}
---
+++ src/ProjectFood/Models/ViewModels/RecipeVM/AllUserRecipesVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectFood.Models.ViewModels.RecipeVM
+{
+    public class AllUserRecipesVM
+    {
+        public List<RecipeVM> expiringRecipes { get; set; }
+        public List<RecipeVM> matchedRecipes { get; set; }
+    }
+}
